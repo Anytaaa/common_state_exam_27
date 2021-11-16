d803acc1619239e7b04f8d89c5582080384d53ac
Anna Sokolova <anna123sok@gmail.com>
2021-11-16 11:37:48 +0300

    вывести только наибольшую сумму

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -13,19 +13,22 @@ void max_sum_div()
     +2. вывести все пары
     +3. вместо пары вывести сумму
     +4. делим сумму проверить
-    5. вывести только наибольшую сумму
+    +5. вывести только наибольшую сумму
     **/
     int n(0);
     ifs >> n;
     vector<int> numbers(n);
     for (int i(0); i<n; i++)
         ifs >> numbers[i];
-    cout << endl;
-
+    int max_sum(0);
     for (int i(0); i<n-1; i++)
-        for (int j(i+1); i<n; i++)
-            if ((numbers[i]  +  numbers[j]) % 2 != 0)
-            cout << numbers[i]  +  numbers[j] << endl;
+        for (int j(i+1); j<n; j++)
+        {
+            int cur_sum(numbers[i] + numbers[j]);
+            if(cur_sum%2 != 0 and cur_sum > max_sum)
+                max_sum = cur_sum;
+        }
+        cout << max_sum << endl;
 
 }
 
