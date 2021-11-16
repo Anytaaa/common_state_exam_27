27cde28cf30762d0e1500fe986e03deeb0f580fa
Anna Sokolova <anna123sok@gmail.com>
2021-11-16 10:57:44 +0300

    вывели все пары

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -7,8 +7,8 @@ void max_sum_div()
 
 {
     /**
-    1. считать и сохранить все числа
-    2. вывести все пары
+    +1. считать и сохранить все числа
+    +2. вывести все пары
     3. вместо пары вывести сумму
     4. делим сумму проверить
     5. вывести только наибольшую сумму
@@ -19,8 +19,10 @@ void max_sum_div()
     for (int i(0); i<n; i++)
         cin >> numbers[i];
     cout << endl;
-    for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
+    for (int j(0); j<n-1; j++)
+        for (int i(j+1); i<n; i++)
+            cout << numbers[j] << " " << numbers[i] << endl;
+
 }
 
 int main()
