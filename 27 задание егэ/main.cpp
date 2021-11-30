6db91a81214e348b845b647a02eb1f54d614bf8e
Anna Sokolova <anna123sok@gmail.com>
2021-11-30 11:39:43 +0300

    найти минимум среди сумм - неэффектиное решение готово

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -16,20 +16,24 @@ void min_sum()
  +1. считать числа
  +2. вывести пары
  +3. вывести пары с минимальным расстоянием
- +4. вместо выводф пары посчитать сумму
- 5. найти минимум среди сумм
+ +4. вместо вывода пары посчитать сумму
+ +5. найти минимум среди сумм
  **/
 
-    int n(0);
-    cin >> n;
+    int n(0),sum_1=0,min_sum=2005 ;
+    cin >> n ;
     vector<int> numbers(n,0);
     for (int i(0); i<n; i++)
         cin >> numbers[i];
     for (int j(0); j<n-1; j++)
         for (int i(j+1); i<n; i++)
             if (i-j>3)
-            cout << numbers[j] << " " << numbers[i] << endl;
-
+        {
+            sum_1 = numbers[j] + numbers[i];
+            if(sum_1<min_sum)
+                min_sum = sum_1;
+        }
+    cout << min_sum;
 }
 
 int main()
