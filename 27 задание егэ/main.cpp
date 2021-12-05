ff11715b156fbd33f8dd6ed87ebc1bd47a39ae27
AnnaBoard <boardgame548@gmail.com>
2021-12-05 16:08:45 +0300

    тест1661 задание 2 готово

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 void
-number_of_22()
+number_of_165()
 {
     /**
     +1) считать числа
     +2) добавить его к одному из счетчиков
-    3) ответ n165*n5+n165*n11+n165*n55+n165*no+n165*(n165-1)/2+n5*n55
+    +3) ответ n165*n3+ n165*n5+n165*n11+n165*n15+n165*n33+n165*n55+n165*no+n165*(n165-1)/2+n15*n11+n15*n55+n15*n33+n33*n55+n3*n55+n5*n33
     **/
 
     int n(0),cur(0);
-    int n22(0),n2(0),n11(0),no(0);
+    int n165(0),n5(0),n15(0),n33(0),n11(0),n55(0),n3(0),no(0);
     cin>>n;
     for (int i(0);i<n;i++)
     {
@@ -22,4 +22,18 @@ number_of_22()
         else if (cur%5==0)n5++;
         else if (cur%55==0)n55++;
         else if (cur%11==0)n11++;
+        else if (cur%3==0)n3++;
+        else if (cur%15==0)n15++;
+        else if (cur%33==0)n33++;
+
+
     }
+    no = n-n165-n5-n55-n11-n3-n15-n33;
+    cout<<
+    n165*n3+ n165*n5+n165*n11+n165*n15+n165*n33+n165*n55+n165*no+n165*(n165-1)/2+n15*n11+n15*n55+n15*n33+n33*n55+n3*n55+n5*n33<<endl;
+}
+int main()
+{
+    number_of_165();
+    return 0;
+}
