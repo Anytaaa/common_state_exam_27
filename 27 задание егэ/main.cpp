080d9e85ed532f782cdb6c02ae9e659bb1aa0486
AnnaBoard <boardgame548@gmail.com>
2021-12-05 15:18:56 +0300

    создали план

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
+void
+number_of_22()
 {
-    cout << "Hello world!" << endl;
-    return 0;
-}
+    /**
+    +1) считать числа
+    +2) добавить его к одному из счетчиков
+    +3) ответ n165*n5+n165*n11+n165*n55+n165*no+n165*(n165-1)/2+n5*n55
+    **/
