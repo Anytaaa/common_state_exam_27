d20469c5cc5b21985452447ffd0f75861dc63ffe
AnnaBoard <boardgame548@gmail.com>
2021-12-15 22:08:33 +0300

    нееффективное решение 2281 задание 2 готово

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 void max_sum_div()
 {
     /**
@@ -9,19 +8,21 @@ void max_sum_div()
     +2)вывести все пары
     +3)вместо пары вывести суммы
     +4)проверить делимость суммы
-    5)вывести только наибольшую сумму
+    +5)вывести только наибольшую сумму
     */
     int n(0);
     cin >> n;
     vector <int> numbers(n);
     for (int i(0); i<n; i++)
-            cin>> numbers[i];
-    cout << endl;
+            cin >> numbers[i];
+    int max_sum(0);
     for (int j(0); j<n-1; j++)
         for (int i(j+1); i<n; i++)
-            if ((numbers[j] + numbers[i]) % 2 != 0)
-            cout << numbers[j] + numbers[i]<< endl;
-
+        {int sum(numbers[j] + numbers[i]);
+            if (sum % 2 != 0 and sum>max_sum)
+                max_sum = sum;
+        }
+    cout <<max_sum;
 }
 int main()
 {
