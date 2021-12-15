8fbfcb9baf05c192586e502ba80903bcf1452ccc
AnnaBoard <boardgame548@gmail.com>
2021-12-15 21:31:04 +0300

    считали числа

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 void div_15()
 {
     /**
-     1)считать числа
+     +1)считать числа
      2)найти наибольшее четное и наибольшее нечетное числа
      3)вывести сумму наибольших чисел
     */
+     int n(0);
+    cin >> n;
+    vector <int> numbers(n);
+    for (int i(0); i<n; i++)
+            cin>> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
+
 }
 
 int main()
 {
-    div_15;
+    div_15();
     return 0;
 }
