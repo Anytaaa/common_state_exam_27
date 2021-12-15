a5e0cf0bbbb09ec17d3ee44ca86b0fc2e56b4a25
AnnaBoard <boardgame548@gmail.com>
2021-12-15 21:41:29 +0300

    тест 2281 задание 1 готово

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -12,24 +12,26 @@ void sum_max_num()
      int n(0);
     cin >> n;
     vector <int> numbers(n);
-    int max_div_2 = -2000;
-    int max_not_div_2 = -2000;
+    int max_div = -2000;
+    int max_not_div = -2000;
     for (int i(0); i<n; i++)
             {
                 cin>> numbers[i];
                 if (numbers[i] % 2 == 0)
-                    {   if (numbers[i] > max_div_2)
-                        max_div_2 = numbers[i];
+                    {   if (numbers[i] > max_div)
+                        max_div = numbers[i];
                     }
 
                 else if (numbers[i] % 2 != 0)
-                        if (numbers[i] > max_not_div_2)
-                            max_not_div_2 = numbers[i];
+                        if (numbers[i] > max_not_div)
+                            max_not_div = numbers[i];
             }
-    cout << max_not_div_2 + max_div_2;
+
+    if (max_not_div + max_div < 0)
+        cout << -1;
+    else cout << max_not_div + max_div;
 
 }
-
 int main()
 {
     sum_max_num();
