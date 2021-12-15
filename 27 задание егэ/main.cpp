60df440f03e4dce811d462d1cb4bd01e5e32bd84
AnnaBoard <boardgame548@gmail.com>
2021-12-15 22:21:43 +0300

    заполнили массив

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -5,13 +5,26 @@ using namespace std;
 void max_sum_div_eff_2()
 {
     /**
-     1)заполнить массив 2 на 40
+     +1)заполнить массив 2 на 40
      2)затем заполнить массив с счетчиками [x > 40][x % 40] += 1
      3)суммировать комплементарные остатки для 40 отдельно, 20 отдельно, остальных отдельно
     */
+    int x = 0;
+    int counters[2][40];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 40; j++)
+        {
+            counters[i][j] = 0;
+        }
+    }
+
+    std::cin >> x;
 }
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div_eff_2();
     return 0;
 }
+
