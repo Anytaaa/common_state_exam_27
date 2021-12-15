0da0e198c8810b798a45fcde456bd7c9fd568cb3
AnnaBoard <boardgame548@gmail.com>
2021-12-15 21:55:06 +0300

    считали все числа

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 void max_sum_div()
 {
     /**
-    1)считать все числа
+    +1)считать все числа
     2)вывести все пары
     3)вместо пары вывести суммы
     4)проверить делимость суммы
     5)вывести только наибольшую сумму
     */
+      int n(0);
+    cin >> n;
+    vector <int> numbers(n);
+    for (int i(0); i<n; i++)
+            cin>> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
+
 }
 int main()
 {
-    cout << "Hello world!";
+    max_sum_div();
     return 0;
 }
