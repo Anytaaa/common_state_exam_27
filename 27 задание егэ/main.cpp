7711a25239d52ae334d990c8fb8a6d121a2ad2f9
AnnaBoard <boardgame548@gmail.com>
2021-12-20 22:40:14 +0300

    вывели все пары

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
-void div_65()
+void div_66()
 {
     /**
-     1) считать и запомнить числа
+     +1) считать и запомнить числа
      2) вывести все пары чисел
      3) вместо пар вывести произведения
      4) проверить делимость произведений
      5) посчиттать и выести коичество подходящих пар
     */
+   int n(0);
+    cin >> n;
+    vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
 }
 
 int main()
 {
-    div_65;
+    div_66();
     return 0;
 }
