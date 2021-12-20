73fc98fb36a3d80e4a8bda03ede3d27523034fb6
AnnaBoard <boardgame548@gmail.com>
2021-12-20 23:00:04 +0300

    считали пары

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void max_sum_div()
+void max_66()
 {
     /**
-    1)считать все числа
+    +1)считать и сохранить все числа
     2) вывести все пары
     3) вместо пары вывести суммы
     4) проверить делимость суммы
     5) вывести только наибольшую сумму
     */
+      int n(0);
+    cin >> n;
+    vector <int> numbers(n);
+    for (int i(0); i<n; i++)
+            cin>> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
+
 }
 int main()
 {
-    cout << "Hello world!";
+    max_66();
     return 0;
 }
 
