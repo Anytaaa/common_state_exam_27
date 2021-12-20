14a56760f2f6ce2a681a87c6752df52ebd323093
AnnaBoard <boardgame548@gmail.com>
2021-12-20 23:34:23 +0300

    создали план

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+bool is_sum(int a, int b)
+{
+    return ((a + b) % 27 == 0);
+}
+
+void num_sum()
+{
+    /*/
+    1)берем первые 7 чисел
+    2) просматриваем их на предмет того, кратны ли они 27
+    3) берем следующие числа от 7 до конца
+    */
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    num_sum();
     return 0;
 }
