9faa73e3c8af3b10eaed77e54752e70c85ad5bfa
AnnaBoard <boardgame548@gmail.com>
2021-12-20 21:51:57 +0300

    Считали числа

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 void div_10()
 {
     /**
-    1) считать числа
+    +1) считать числа
     2) добавить его к одному из счетчиков
-    3) ответ n10*n5+n10*n2+n10*no+n10(n10-1)/2*n10*n2
+    3) ответ n22*n11+n22*n2+n22*no+n22(n22-1)/2*n11*n2
     **/
+   int n(0);
+    cin >> n;
+    vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
 }
 
 int main()
 {
-    div_10;
+    div_10();
     return 0;
 }
