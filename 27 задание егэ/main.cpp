54b96d958a980307ead64a9cf51fd2326e64aa18
AnnaBoard <boardgame548@gmail.com>
2021-12-20 22:25:59 +0300

    добавить его к одному из счетчиков

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -15,11 +15,21 @@ void div_10()
 int n10 = 0, n5 = 0, n2 = 0, no = 0;
    int n = 0, cur = 0;
     cin >> n;
-    for (int i (0); i<n; i++)
-        {
-            cin >> cur;
-            cout << cur << ' ';
-        }
+    for (int i = 0; i<n; i++)
+    {
+        cin >> cur;
+
+        if (cur % 10 == 0)
+            n6++;
+        else if (cur % 2 ==0)
+            n2++;
+        else if (cur % 5 == 0)
+            n3++;
+    }
+
+    no = n - n10 - n5 - n2;
+    cout << n10 << ' ' << n5 << ' ' << n2 << ' ' <<  no;
+
 
 }
 
