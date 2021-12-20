4a0af19c4d7b472c3afe8efe3ba905c54309b2d1
AnnaBoard <boardgame548@gmail.com>
2021-12-20 22:24:23 +0300

    считали числа

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -12,6 +12,17 @@ void div_10()
     **/
 }
 
+int n10 = 0, n5 = 0, n2 = 0, no = 0;
+   int n = 0, cur = 0;
+    cin >> n;
+    for (int i (0); i<n; i++)
+        {
+            cin >> cur;
+            cout << cur << ' ';
+        }
+
+}
+
 int main()
 {
     div_10();
