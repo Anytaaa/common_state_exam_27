a230625a36ee94ebea8a72f028bc9ff40f415ce5
AnnaBoard <boardgame548@gmail.com>
2021-12-20 22:21:26 +0300

    создали план

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void div_10()
+{
+    /**
+    +1) считать числа
+    +2) добавить его к одному из счетчиков
+    +3) ответ n10*n5+n10*n2+n10*no+n10(n10-1)/2*n5*n2
+    **/
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    div_10();
     return 0;
 }
