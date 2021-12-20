8b1b69f74d30918d9244df6b4d2822ffeca0d9d8
AnnaBoard <boardgame548@gmail.com>
2021-12-20 22:43:59 +0300

    вывели пары с минимальным расстоянием

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-void div_66()
+
+void  min_66()
 {
     /**
-     +1) считать и запомнить числа
-     2) вывести все пары чисел
-     3) вместо пар вывести произведения
-     4) проверить делимость произведений
-     5) посчиттать и выести коичество подходящих пар
-    */
-   int n(0);
+
+    + 1) считать числа
+    + 2) вывести пары
+    3) вывести пары с минимальным расстоянием
+    4) вместо вывода пары подсчитать сумму
+    5) найти минимум среди сумм
+    **/
+
+    int n;
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
         cin >> numbers[i];
-    for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
+    for (int j(0); j<n-1; j++)
+        for (int i(j+1); i<n; i++)
+            cout << numbers[j]<<" "<<numbers[i]<< endl;
 }
 
 int main()
 {
-    div_66();
+    min_66();
     return 0;
 }
