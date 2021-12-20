6cf6246cee686660b71689d3902bf7a5e1f94f4a
AnnaBoard <boardgame548@gmail.com>
2021-12-20 21:54:00 +0300

    вывели эти пары

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 void div_10()
 {
-    /**
+   /**
     +1) считать числа
-    2) добавить его к одному из счетчиков
+    +2) добавить его к одному из счетчиков
     3) ответ n22*n11+n22*n2+n22*no+n22(n22-1)/2*n11*n2
     **/
    int n(0);
@@ -14,8 +14,9 @@ void div_10()
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
         cin >> numbers[i];
-    for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
+    for (int j = 0; j < n; ++j)
+        for (int i = j + 1; i < n; ++i)
+            cout << numbers[j] << ' ' << numbers[i] << endl;
 }
 
 int main()
