245633698348d999aad5e9e51b7918edc1cf8b8a
AnnaBoard <boardgame548@gmail.com>
2021-12-20 21:56:15 +0300

    добавить его к одному из счетчиков

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 
 using namespace std;
-void div_10()
+void div_15()
 {
-   /**
+    /**
     +1) считать числа
     +2) добавить его к одному из счетчиков
-    3) ответ n22*n11+n22*n2+n22*no+n22(n22-1)/2*n11*n2
+    +3) ответ n22*n11+n22*n2+n22*no+n22(n22-1)/2*n11*n2
     **/
    int n(0);
     cin >> n;
@@ -16,11 +16,11 @@ void div_10()
         cin >> numbers[i];
     for (int j = 0; j < n; ++j)
         for (int i = j + 1; i < n; ++i)
-            cout << numbers[j] << ' ' << numbers[i] << endl;
+            cout << numbers[j] * numbers[i] << endl;
 }
 
 int main()
 {
-    div_10();
+    div_15();
     return 0;
 }
