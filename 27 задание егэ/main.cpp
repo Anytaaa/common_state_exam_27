c24a69fae426dfe580f8861349bcca28e556d8de
AnnaBoard <boardgame548@gmail.com>
2021-12-20 23:11:12 +0300

    эффективное решение теста 1661 задание 2 готово

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -5,20 +5,27 @@ using namespace std;
 void div_66()
 {
     /**
-     +1) считать числа
+     +1) считать  числа
      +2) вывести все пары чисел
      +3) вместо пар вывести произведения
-     4) проверить делимость произведений
-     5) посчиттать и выести коичество подходящих пар
+     +4) проверить делимость произведений
+     +5) посчиттать и выести коичество подходящих пар
     */
    int n(0);
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
         cin >> numbers[i];
+
+    int good_pair = 0;
     for (int j = 0; j < n; ++j)
         for (int i = j + 1; i < n; ++i)
-            cout << numbers[j] * numbers[i] << endl;
+            {
+                int cur_prod = numbers[j] * numbers[i];
+                if (cur_prod % 66 == 0)
+                    good_pair++;
+            }
+    cout << good_pair;
 }
 
 int main()
