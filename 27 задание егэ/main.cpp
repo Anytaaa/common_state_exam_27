7a54a81ebbe751b96d23300df9929903429ad6f6
AnnaBoard <boardgame548@gmail.com>
2021-12-20 23:25:36 +0300

    неэффективное решение 2318 1 готово

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -9,7 +9,7 @@ void num_sum()
      +1)считать последовательность чисел
      +2) вывести пары удовлетворяющие первому условию
      +3) вывести пары удовлетворяющие второму условию
-     4) вывести ответ
+     +4) вывести ответ
     */
    int n;
    cin >> n;
@@ -17,12 +17,14 @@ void num_sum()
     for (int i(0); i < n; i++)
         cin >> numbers[i];
 
+    int count = 0;
    for (int i = 0; i < n; ++i)
     for (int j = i + 1; j < n; ++j)
     {
-        if ((abs(i - j) < 7 and ((numbers[i] + numbers[j]) % 27 == 0))
-            cout << numbers[i] << ' ' << numbers[j] << endl;
+        if ((abs(i - j) < 7) and ((numbers[i] + numbers[j]) % 27 == 0))
+            count += 1;
     }
+    cout << count;
 }
 int main()
 {
