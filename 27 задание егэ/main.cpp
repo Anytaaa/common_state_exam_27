b11564c87b0b8987a744370350f3a2539244aaca
AnnaBoard <boardgame548@gmail.com>
2021-12-20 22:56:47 +0300

    неэффективное решение 1661 2 задание готово

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-void  min_66()
+using namespace std;
+void div_66()
 {
     /**
-
-    + 1) считать числа
-    + 2) вывести пары
-    + 3) вывести пары с минимальным расстоянием
-    4) вместо вывода пары подсчитать сумму
-    5) найти минимум среди сумм
-    **/
-
-    int n;
+     +1) считать и запомнить числа
+     +2) вывести все пары чисел
+     +3) вместо пар вывести произведения
+     +4) проверить делимость произведений
+     +5) посчиттать и выести коичество подходящих пар
+    */
+   int n(0);
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
         cin >> numbers[i];
-    for (int j(0); j<n-1; j++)
-        for (int i(j+1); i<n; i++)
-            if (i - j > 3)
-                cout << numbers[j] << " " <<numbers[i] << endl;
+
+    int good_pair= 0;
+    for (int j = 0; j < n; ++j)
+        for (int i = j + 1; i < n; ++i)
+            {
+                int cur_prod = numbers[j] * numbers[i];
+                if (cur_prod % 91 == 0)
+                    good_pair++;
+            }
+    cout << good_pair;
 }
 
 int main()
 {
-    min_66();
+    div_66();
     return 0;
 }
