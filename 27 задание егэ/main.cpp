4314d02ac54d4191deca66cd1852e9753f9b68b7
Anna Sokolova <anna123sok@gmail.com>
2021-12-21 11:25:28 +0300

    создали план

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -2,52 +2,18 @@
 
 using namespace std;
 
-void max_sum_div_eff_2()
+void test_a()
 {
     /**
-     +1)заполнить массив 2 на 40
-     +2)затем заполнить массив с счетчиками [x > 40][x % 40] += 1
-     +3)суммировать комплементарные остатки для 40 отдельно, 20 отдельно, остальных отдельно
+    1) ввести числа
+    2) вычеслить количество троек различных элементов
+    3) понять делится лиэто количество на 4
+    4) подсчет контрольной сумммы
+
     */
-    int x = 0;
-    int meter[2][40];
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 40; j++)
-        {
-            meter[i][j] = 0;
-        }
-    }
-    cin>> x;
-    while (x != -9999)
-    {
-        meter[x > 40][x % 40] += 1;
-        cin>> x;
-    }
 
-    int sum = 0;
-    int big_40 = meter[1][0], smal_40 = meter[0][0];
-    sum += big_40 * (big_40 - 1) / 2;
-    sum += big_40 * smal_40;
-    big_40 = meter[1][20];
-    smal_40 = meter[0][20];
-    sum+= big_40 * (big_40 - 1) / 2;
-    sum += big_40 * smal_40;
-    for (int i = 1; i < 20; ++i)
-    {
-        int count_complimentary_num_big_40 = meter[1][40 - i];
-        big_40 = meter[1][i];
-        int count_complimentary_num_smal_40 = meter[0][40 - i];
-        smal_40 = meter[0][i];
-        sum += big_40 * count_complimentary_num_big_40;
-        sum += big_40 * count_complimentary_num_smal_40;
-        sum += smal_40 * count_complimentary_num_big_40;
-    }
-
-    cout << sum;
-}
 int main()
 {
-    max_sum_div_eff_2();
+    test_a();
     return 0 ;
 }
