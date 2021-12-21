17d84b85555a993d3648a3cd1f6b2d5281d06e37
Anna Sokolova <anna123sok@gmail.com>
2021-12-21 11:47:22 +0300

    +вычеслили количество троек различных элементов

--- 27 задание егэ/main.cpp
+++ 27 задание егэ/main.cpp
@@ -5,21 +5,24 @@ using namespace std;
 void test_a()
 {
     /**
-    +1) ввести числа
-    2) вычеслить количество троек различных элементов
-    3) понять делится лиэто количество на 4
-    4) подсчет контрольной сумммы
+    +1)ввести числа
+    +2)вычеслить количество троек различных элементов
+    3)понять делится лиэто количество на 4
+    4)подсчет контрольной сумммы
 
     */
 
-int n(0);
+ int n(0);
     cin >> n;
     vector <int> numbers(n);
     for (int i(0); i<n; i++)
             cin>> numbers[i];
     cout << endl;
-    for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
+    for (int j(0); j<n-1; j++)
+        for (int i(j+1); i<n; i++)
+            for (int i(i+1);i<n;i++)
+            cout << numbers[j]<<" "<<numbers[i]<< endl;
+
 
 }
 int main()
